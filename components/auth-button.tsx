b1760799e3bbe3148d2139e3e62e6083392d64d9
fruitsdrink <fruitsdrink@126.com>
2025-11-03 16:47:12 +0800

    Add authentication support with NextAuth, integrating Google provider and Prisma adapter. Update User model in Prisma schema to include additional fields for accounts and sessions. Enhance layout with Providers component for session management.

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import { signIn } from "next-auth/react";
 import { Button } from "./ui/button";
 
 export const AuthButton = () => {
-  return <Button variant={"outline"}>Sign In</Button>;
+  return (
+    <Button variant={"outline"} onClick={() => signIn("google")}>
+      Sign In
+    </Button>
+  );
 };
---
+++ lib/auth.ts
@@ -0,0 +1,23 @@
+// import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "./prisma";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  // Configure one or more authentication providers
+  providers: [
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID!,
+    //   clientSecret: process.env.GITHUB_SECRET!,
+    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      httpOptions: {
+        timeout: 10000,
+      },
+    }),
+  ],
+};
