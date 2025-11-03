de60f311f6a2925affc8c1763c8f1d1b68071677
fruitsdrink <fruitsdrink@126.com>
2025-11-03 23:58:14 +0800

    Implement authentication dropdown menu with Radix UI components, update AuthButton for session management, and add GitHub provider to auth options. Introduce new dropdown menu component for enhanced UI interactions.

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,12 +1,59 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export const AuthButton = () => {
+  const { data: session, status } = useSession();
+  if (status === "loading") {
+    return (
+      <Button variant={"ghost"} disabled>
+        Loading...
+      </Button>
+    );
+  }
+  if (!session) {
+    return (
+      <Button variant={"outline"} onClick={() => signIn("github")}>
+        Sign In
+      </Button>
+    );
+  }
+
   return (
-    <Button variant={"outline"} onClick={() => signIn("google")}>
-      Sign In
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"ghost"} className="relative h-8 w-8 rounded-full">
+          <Avatar className="size-8">
+            <AvatarImage src={session.user?.image || ""} />
+            <AvatarFallback>
+              {session.user?.name?.charAt(0).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuItem className="font-normal">
+          <div className="flex flex-col spy1'">
+            <p className="text-sm font-medium leading-none">
+              {session.user?.name}
+            </p>
+            <p className="text-xs text-muted-foreground leading-none">
+              {session.user?.email}
+            </p>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
